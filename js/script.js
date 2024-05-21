3611b0f13c44a3e029dae8900a03c3f15a933097
LevanMachitadze <160760563+LevanMachitadze@users.noreply.github.com>
2024-05-21 14:52:12 +0400

    finish
    finishing project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
 const url = 'https://api.quotable.io/random';
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    let quote = document.getElementById('quote');
-    let author = document.getElementById('author ');
-    quote.textContent = data.content;
-    author.textContent = data.author;
+    quote.textContent = ` "${data.content}"`;
+    author.textContent = `-${data.author}`;
     console.log(data.content);
     console.log(data.author);
   });
